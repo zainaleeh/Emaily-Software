854b93b5ad01dc7cf6782ec87a0f8acee4e6700e
Zain Ali <137424919+zainaleeh@users.noreply.github.com>
2024-06-11 16:17:32 -0400

    updated logout

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -12,6 +12,11 @@ module.exports= app => {
   //google callback handler
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();//kills the id with the specific cookie hence logs out
+    res.send(req.user);
+
+  });
   // handler to handle cookie requests, give back the user id for the person who is already logged in
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
