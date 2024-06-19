0c904a6ab3377cbf1ffa61afcd8362cee6034bbf
Zain Ali <137424919+zainaleeh@users.noreply.github.com>
2024-06-19 19:30:46 -0400

    Update app.js

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'; 
 import {connect} from 'react-redux';
 import * as actions from '../actions';
+
 //brouseRouter is the brain of the router
 //route sets rules
 
