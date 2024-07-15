d22ae45a777aa38de3772f332f2b3338e5db5cb9
Zain Ali <137424919+zainaleeh@users.noreply.github.com>
2024-07-15 15:08:33 +0500

    section 12 completed

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,8 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+
+
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -10,9 +15,56 @@ const Survey = mongoose.model('surveys');
 
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+
+  //fetching the surveys for each specific user without fetching the recipients
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false,
+    });
+
+    res.send(surveys);
+  });
+
+  
+
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting');
   });
+
+  //extracting the valueable information
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => { //this is the query to update the database
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false },
+            },
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
+      .value();
+
+    res.send({});
+  });
+
+
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const {title, subject, body, recipients} = req.body; //this is what user will pass on as a request when coming to api/surveys
