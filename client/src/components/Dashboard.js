cd2e8e1e95e22c91868cdf5f1936401b34b8a912
Asif Khan <asiff080@gmail.com>
2024-07-25 12:40:33 +0500

    Survery List component changed

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,76 +6,17 @@ const Dashboard = () => {
     return (
         <section id="service" class="marketing_content_area section-padding">
             <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="single_feature_one">
-                            <div class="sf_top">
-                                <span class="ti-flag ss_one"></span>
-                                <h2><a href="#" data-bs-toggle="modal" data-bs-target="#serviceModal">Digital
-                                    <br />Marketing</a></h2>
-                            </div>
-                            <p>Ajker modhe order confirm korun. na hole pore postaben. stock shimito. deri korle late hobe.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="single_feature_one">
-                            <div class="sf_top">
-                                <span class="ti-pencil-alt ss_two"></span>
-                                <h2><a href="#" data-bs-toggle="modal" data-bs-target="#serviceModal2">Create
-                                    <br />Content</a></h2>
-                            </div>
-                            <p>Ajker modhe order confirm korun. na hole pore postaben. stock shimito. deri korle late hobe.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="single_feature_one">
-                            <div class="sf_top">
-                                <span class="ti-light-bulb ss_three"></span>
-                                <h2><a href="#" data-bs-toggle="modal" data-bs-target="#serviceModal3">UI/UX
-                                    <br />Design</a></h2>
-                            </div>
-                            <p>Ajker modhe order confirm korun. na hole pore postaben. stock shimito. deri korle late hobe.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="single_feature_one">
-                            <div class="sf_top">
-                                <span class="ti-desktop ss_four"></span>
-                                <h2><a href="#" data-bs-toggle="modal" data-bs-target="#serviceModal4">Website <br />
-                                    Design</a></h2>
-                            </div>
-                            <p>Ajker modhe order confirm korun. na hole pore postaben. stock shimito. deri korle late hobe.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="single_feature_one">
-                            <div class="sf_top">
-                                <span class="ti-world ss_five"></span>
-                                <h2><a href="#" data-bs-toggle="modal" data-bs-target="#serviceModal5">Web Development</a>
-                                </h2>
-                            </div>
-                            <p>Ajker modhe order confirm korun. na hole pore postaben. stock shimito. deri korle late hobe.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12">
-                        <div class="single_feature_one">
-                            <div class="sf_top">
-                                <span class="ti-package ss_six"></span>
-                                <h2><a href="#" data-bs-toggle="modal" data-bs-target="#serviceModal6">App Development</a>
-                                </h2>
-                            </div>
-                            <p>Ajker modhe order confirm korun. na hole pore postaben. stock shimito. deri korle late hobe.
-                            </p>
-                        </div>
-                    </div>
+                <div class="row mt-2">
+                    <SurveyList />
                 </div>
             </div>
+            <div className="fixed-action-btn">
+                <Link to="/surveys/new" className="btn-floating btn-large">
+                    <i class="fa fa-plus" aria-hidden="true"></i>
+                </Link>
+            </div>
         </section>
+
     );
 };
 
